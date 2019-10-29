9fe869b73860b6b6191613edf9e4ceb48e2f7815
new-programer <insanity2017@126.com>
2019-10-29 11:37:15 +0800

    a repository for reviewing java

---
+++ src/exercises/dataOperation/GenerateSingleModule.java
@@ -0,0 +1,17 @@
+package exercises.dataOperation;
+
+import java.util.*;
+
+public class GenerateSingleModule {
+    public static void main(String[] args) {
+        String readPath = "src/resources/out.csv";
+        String writePath = "src/results"+"module.csv";
+
+        List list= readData(readPath);
+    }
+
+    private static List readData(String readPath) {
+        List list = null;
+        return null;
+    }
+}
